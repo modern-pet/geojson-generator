024bbb7c3d9fa048c681eb064bc66eb793bec386
jkwik <jkwikcs@gmail.com>
2024-05-16 14:40:19 +0700

    Revert "Correct the spacing for setiabudi and pulogadung"
    This reverts commit bce4c1322597c6e1fc7df5d50700a4b5cd957ed0.

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ jakartaData.forEach(entry => {
   }
 });
 
-// Function to create GeoJSON features for each kecamatan
+// Function to create GeoJSON features for each zip code
 const createGeoJsonFeatures = (geoJsonData, zipCodeMapping) => {
   const features = [];
 
@@ -37,21 +37,23 @@ const createGeoJsonFeatures = (geoJsonData, zipCodeMapping) => {
     const zipCodes = zipCodeMapping[kecamatanName];
 
     if (zipCodes) {
-      const newFeature = {
-        type: "Feature",
-        properties: {
-          zip_codes: zipCodes,
-          name: feature.properties.name
-        },
-        geometry: feature.geometry
-      };
-      features.push(newFeature);
+      zipCodes.forEach(zipCode => {
+        const newFeature = {
+          type: "Feature",
+          properties: {
+            zip: zipCode,
+            name: feature.properties.name
+          },
+          geometry: feature.geometry
+        };
+        features.push(newFeature);
+      });
     } else {
       // Add the kecamatan even if there are no zip codes
       const newFeature = {
         type: "Feature",
         properties: {
-          zip_codes: [],
+          zip: null,
           name: feature.properties.name
         },
         geometry: feature.geometry
